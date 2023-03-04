6cd50c88dd393f455847f49397e93e20c70199ba
devhenode <devodehenry.2000@gmail.com>
2023-03-03 22:01:01 -0800

    Task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+include "main.h"
+
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int num;
+	char la;
+
+	for (la = 'a'; la <= 'z'; la++)
+	{
+		if (num < 10)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
