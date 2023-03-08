787b9e088e6bf26f9e8b7e74d248686074e5049e
devhenode <devodehenry.2000@gmail.com>
2023-03-08 06:49:32 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers 0-9
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int num;
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar("%d", num);
+	}
+	_putchar("\n");
+}
