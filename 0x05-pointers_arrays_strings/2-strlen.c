ae289f5cb4e755599864c565d3460f6c0e3683b1
devhenode <devodehenry.2000@gmail.com>
2023-03-13 21:42:26 -0700

    Task 2

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,3 +1,19 @@
 #include "main.h"
 
+/**
+  * _strlen - Returns the length of a string
+  * @s: String to count
+  *
+  * Return: String length
+  */
+int _strlen(char *s)
+{
+	int stg = 0;
 
+	while (*s != '\0')
+	{
+		*s++;
+		stg++;
+	}
+	return (stg);
+}
