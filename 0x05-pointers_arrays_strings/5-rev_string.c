43701c936d3fa85d6345912c0c404096034c9667
devhenode <devodehenry.2000@gmail.com>
2023-03-15 05:43:48 -0700

    Task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * rev_string - reverse string
+ * @s: string
+ * Return: void
+ */
+
+void rev_string(char *s)
+{
+	int i;
+	for (i = strlen(s) - 1; i >= 0; i--)
+		s[i];
+}
