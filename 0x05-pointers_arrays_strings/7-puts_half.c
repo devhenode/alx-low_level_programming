6deb326c1e1925ae3d3383682dc10047de181eca
devhenode <devodehenry.2000@gmail.com>
2023-03-21 15:23:49 -0700

    Task 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,24 +11,24 @@ void puts_half(char *str)
 {
 	int i;
 	int j = 0;
-	int ave = j / 2;
-	char last = ave - 1;
-	int res = 0;
 
 	while (str[j] != '\0')
 		j++;
 
 	if (str[j] % 2 != 0)
 	{
-		last++;
+		i = (i - 1) / 2;
+		i++;
 	}
 	else
 	{
-		last / 2;
+		k = j / 2;
 	}
-	for (; last < j; last++)
-	{
-		_putchar(str[last]);}
 
+	for (; k < j; k++)
+	{
+		_putchar(str[k]);
+	}
 	_putchar('\n');
+	
 }
