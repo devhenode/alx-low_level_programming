229ecfe29b249562d0f15b70810231a1de60281a
devhenode <devodehenry.2000@gmail.com>
2023-03-21 16:48:27 -0700

    Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+  * _strcpy - Copy a string
+  * @dest: Destination value
+  * @src: Source value
+  *
+  * Return: the pointer to dest
+  */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
