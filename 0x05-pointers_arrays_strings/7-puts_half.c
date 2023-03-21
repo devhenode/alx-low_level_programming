4ea201cb8e2002accd028af4df0e10d94f2930d4
devhenode <devodehenry.2000@gmail.com>
2023-03-21 13:59:15 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+  * puts_half - Prints half of a string
+  * @str: The string to print
+  *
+  * Return: void
+  */
+
+void puts_half(char *str)
+{
+	int i;
+	int j = 0;
+	int ave = j / 2;
+	char last = ave - 1;
+	int res = 0;
+
+	while (str[j] != '\0')
+		j++;
+
+	if (str[j] % 2 !== 0)
+	{
+		last++;
+	}
+	else
+	{
+		last / 2;
+	}
+	for (; last < j; last++)
+	{
+		_putchar(str[last]);
+	}
+
+	_putchar('\n');
+}
