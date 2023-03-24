25cb892d69630001bb5359ab6a39dc6ee820e72e
devhenode <devodehenry.2000@gmail.com>
2023-03-23 19:13:58 -0700

    Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+  * _strcmp - Compares two strings
+  * @s1: The first string
+  * @s2: The second string
+  *
+  * Return: int value
+  */
+
+int _strcmp(char *s1, char *s2)
+{
+	int i;
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		if (s1[i] == s2)
+		{
+			return (0);
+		}
+		else if (s1[i] < s2)
+		{
+			return (-1);
+		}
+		else if (s1[i] > s2)
+		{
+			return (+1);
+		}
+		else
+			return (NULL);
+	}
+
+	return (0);
+}
