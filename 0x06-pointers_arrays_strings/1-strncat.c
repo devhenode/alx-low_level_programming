71c982d2948288c13a38329bdbbe951353884da7
devhenode <devodehenry.2000@gmail.com>
2023-03-23 17:55:42 -0700

    Task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenate two strings
+ * @dest: char pointer string
+ * @src: char pointer string
+ * @n: number of elements to concatenate
+ * Return: pointer to resulting `dest`
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+		i++;
+
+	for (j = 0; src[j] != '\0' && n > 0; j++, n--)
+	{
+		dest[i] = src[j];
+	}
+
+	return (dest);
+}
