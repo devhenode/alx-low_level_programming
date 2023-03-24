5d93f277f1a254a8891f4353a71efe844cba548e
devhenode <devodehenry.2000@gmail.com>
2023-03-23 20:25:13 -0700

    Task 3

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -12,22 +12,10 @@ int _strcmp(char *s1, char *s2)
 {
 	int i;
 
-	for (i = 0; s1[i] != '\0' && s[2] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
-		if (s1[i] == s2[i])
-		{
-			return (0);
-		}
-		else if (s1[i] < s2[i])
-		{
-			return (-1);
-		}
-		else if (s1[i] > s2[i])
-		{
-			return (+1);
-		}
-		else
-			return (NULL);
+		if (s[i] != s2[i])
+			return (s1[i] - s2[i]);
 	}
 
 	return (0);
